ca812377ad7272dcafbeda46cd5487693982e4d8
Sven Suursoho <sven@alt.ee>
2018-10-21 15:34:45 +0300

    Pass username to setup.sh

--- ubuntu-server/setup.sh
+++ ubuntu-server/setup.sh
@@ -2,18 +2,16 @@
 
 set -e
 
-logger -t setup-env "Update APT"
+test -n "$1" -a -d "/home/$1" || exit 1
+
 sudo apt-get -y update
 sudo apt-get -y upgrade
-
-logger -t setup-env "Install apps"
 sudo apt-get -y install zsh tmux
 
-logger -t setup-env "Setup env"
-cd ~
-homerepo=https://raw.githubusercontent.com/svens/home/master
-curl -o .aliases $homerepo/aliases
-curl -o .env $homerepo/env
-curl -o .tmux.conf $homerepo/tmux.conf
-curl -o .zshrc $homerepo/zshrc
-sudo chsh -s /bin/zsh $(whoami)
+user=$1
+repo=https://raw.githubusercontent.com/svens/home/master
+curl --silent --output /home/$user/.aliases $repo/aliases
+curl --silent --output /home/$user/.env $repo/env
+curl --silent --output /home/$user/.tmux.conf $repo/tmux.conf
+curl --silent --output /home/$user/.zshrc $repo/zshrc
+sudo chsh -s /bin/zsh $user
